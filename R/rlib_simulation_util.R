26579e3a53adce279c275c1a19d7b8d4aa1e37de
Yanyu Liang <yanyul@uchicago.edu>
2019-12-02 18:21:41 -0600

    added simulation for multi-snp model

--- R/rlib_simulation_util.R
+++ R/rlib_simulation_util.R
@@ -1,3 +1,77 @@
+#' Generate a gene instance for simulation
+#'
+#' This function generates a gene region to work with.
+#' For a gene region, the following parameters are required
+#'   1. L_gene: length of gene region;
+#'   2. library_dist: distribution of library size (e.g. NegBionmial and Poisson);
+#'   3. theta_dist: distribution of theta_{0,i} (e.g. Beta);
+#'   4. f: frequency of polymorphic site along gene body;
+#'   5. maf: MAF (a range) of polymorphic site;
+#'   6. p: probability of having reads by position.
+#'
+#' @param L_gene length of gene body
+#' @param library_dist distribution of library size, a list of 'type' (can be either 'poisson' or 'negbinom') and corresponding parameters: 1. 'negbinom': 'prob' and 'size'; 2. 'poisson': 'lambda'
+#' @param theta_dist distribution of baseline abundance theta_{0,i}, a list of 'type' (can be either 'beta' or 'lognormal') and corresponding parameters: 1. 'beta': 'alpha' and 'beta'; 2. 'lognormal': 'k' (mu) and 'sigma'
+#' @param f frequency of polymorphic site along the gene body
+#' @param maf allele frequency range of polymorphic site (MAF ~ Unif(maf_low, maf_high))
+#' @param p probability of having read at each position along the gene body
+#'
+#' @return an gene instance
+#'
+#' @examples
+#' create_gene()
+#'
+#' @export
+#' @importFrom stats rbinom runif
+create_gene = function(
+  L_gene = 1e4,
+  library_dist = list(type = 'negbinom', prob = 1.6e-7, size = 15),
+  theta_dist = list(type = 'beta', mu = 1e-5, sd = 0.5e-5),
+  f = 0.001,
+  maf = c(0.05, 0.3),
+  p = rep(1 / 1e4, 1e4)
+) {
+  if(theta_dist$type == 'beta') {
+    theta_i_params = guess_alpha_beta_from_mu_sd(theta_dist$mu, theta_dist$sd)
+  } else if(theta_dist$type == 'lognormal') {
+    theta_i_params = guess_k_sigma_from_mu_sd(theta_dist$mu, theta_dist$sd)
+  }
+  # list(L_gene = L_gene, library_dist = library_dist, theta_i_params = theta_i_params, f = f, maf = maf, p = p)
+  generate_gene_instance(L_gene, library_dist, theta_i_params, f, maf, p)
+}
+
+#' Generate genotype (for single SNP model only)
+#'
+#' Given MAF range and sample size, generate dosage genotype (0, 1, 2) for one site.
+#'
+#' @param maf MAF range, c(maf_low, maf_high)
+#' @param nsample number of individual to simulate
+#' @param nreplicate number of replicates to simulation
+#'
+#' @return a list of length nreplicate where each element contains a data.frame of simulation genotype (for each haplotype) along with allele frequency.
+#'
+#' @examples
+#' create_genotype(
+#'   maf = c(0.05, 0.3),
+#'   nsample = 300,
+#'   nreplicate = 1000
+#' )
+#' @export
+#' @importFrom stats rbinom
+create_genotype = function(maf, nsample, nreplicate) {
+  M = nreplicate
+  N = nsample
+  out = list()
+  for(m in 1 : M) {  # iterate over replicates
+    fG = runif(1, min = maf[1], max = maf[2])
+    h1 = rbinom(N, 1, fG)
+    h2 = rbinom(N, 1, fG)
+    genotype = data.frame(h1 = h1, h2 = h2)
+    out[[length(out) + 1]] = list(genotype = genotype, fG = fG)
+  }
+  out
+}
+
 generate_gene_instance = function(L_gene, library_dist, theta, f, maf, vec_pos) {
 
   ## This function generates a gene region to work with
